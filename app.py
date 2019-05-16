a18647c8df44b9671443c3bd7fd1f5b96d8ab064
Roman Shkapiak <r.shkapiak@gmail.com>
2019-05-16 15:45:25 +0300

    Change app.py for manual creation Project and Version in Redmine. Add Roadmap version for all issues which being created

--- app.py
+++ app.py
@@ -1,12 +1,33 @@
 """
 Main application
 """
+import sys
 import from_spreadsheet
 import to_redmine
-import config
 
-PROJECT_NAME = str(input('Input name of Redmine project\n>'))
-VERSION = str(input('Input name of Redmine Roadmap version\n>'))
+project = to_redmine.get_redmine().project.all()  # get existing Redmine projects
+project_dict = {}
+for i in list(project):
+    project_dict[i.name] = i.identifier
+PROJECT_NAME = str(input('Input the of Redmine project\n>'))
+if PROJECT_NAME in project_dict:
+    PROJECT_ID = project_dict[PROJECT_NAME]
+else:
+    sys.exit('There is not such project in Redmine. Create it first')
+
+project = to_redmine.get_redmine().project.get(PROJECT_ID)
+if not project.versions:
+    sys.exit('Create the Roadmap version in selected Redmine Project')
+version_dict = {}
+for i in list(project.versions):  # get versions from selected project
+    version_dict[i.name] = i.id
+VERSION_NAME = str(input('Input name of Redmine Roadmap version\n>'))
+if VERSION_NAME in version_dict:
+    VERSION_ID = version_dict[VERSION_NAME]
+else:
+    sys.exit('There is not such Roadmap version. Create it first')
+VERSION_ID = version_dict[VERSION_NAME]
+
 SPREADSHEET_URL = str(input('Input spreadsheet url\n>'))
 
 
@@ -15,10 +36,9 @@ class App:
     Class for application
     """
     def __init__(self):
-        self.project_name = PROJECT_NAME
-        self.version = VERSION
+        self.project_id = PROJECT_ID
+        self.version = VERSION_ID
         self.spreadsheet_url = SPREADSHEET_URL
-        self.project_id = self.project_name.lower().replace(' ', '-')
 
     @staticmethod
     def create_issue_id(project_id, issue_name, version, time, parent_issue_id):
@@ -45,37 +65,22 @@ class App:
         data = from_spreadsheet.get_spreadsheet(self.spreadsheet_url)
         # get data from spreadsheet
 
-        to_redmine.create_project(self.project_name, config.REDMINE_URL)  # create project
-
-        version_dict = {}
-        if self.version:  # create Roadmap version
-            to_redmine.create_version(self.project_id, self.version)
-            project = to_redmine.get_redmine().project.get(self.project_id)
-            for i in list(project.versions):
-                version_dict[i.name] = i.id
-
         for row in data:
             if row[0]:
                 issue_name = row[0]
-                version = 0
                 time = row[-1]
                 parent_issue_id = ''
                 parent_issue_id_root = self.create_issue_id(self.project_id, issue_name,
-                                                            version, time, parent_issue_id)
+                                                            self.version, time, parent_issue_id)
             if not row[0] and row[1]:
                 issue_name = row[1]
-                version = 0
                 time = row[-1]
                 parent_issue_id_child = self.create_issue_id(self.project_id, issue_name,
-                                                             version, time, parent_issue_id_root)
+                                                             self.version, time, parent_issue_id_root)
             if not row[0] and not row[1] and row[2]:
                 issue_name = row[2]
                 time = row[-1]
-                if self.version:  # check if there is no Roadmap version
-                    version = version_dict[self.version]
-                else:
-                    version = 0
-                to_redmine.create_issue(self.project_id, issue_name, version,
+                to_redmine.create_issue(self.project_id, issue_name, self.version,
                                         time, parent_issue_id_child)
             else:
                 continue
